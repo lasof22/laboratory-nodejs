b2f14836b060d9dc5b807f657080669fff2f2247
Laura <lsralaura.romero@gmail.com>
2022-10-25 19:54:05 -0500

    add user authentication

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var database = require("./config/database");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var auth = require("./auth/main_auth");
+
 var empleadosRouter = require('./routes/empleados.router');
 var serviciosRouter = require('./routes/servicios.router');
+var usuariosRouter = require('./routes/usuarios.router');
 
 var app = express();
 
@@ -21,12 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // mongo connect
 database.mongoConnect();
-
+app.use(auth);
 // routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/empleados', empleadosRouter);
 app.use('/servicios', serviciosRouter);
+app.use('/usuarios', usuariosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/usuarios.model.js
@@ -0,0 +1,10 @@
+//creacion de una tabla - para crear otra se debe crear un archivo nombre.model.js dentro de models
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UsuariosSchema = new Schema({
+    usuario:{type: String, required: true, max:60},
+    pass:{type: String, required: true, max:128},
+});
+
+module.exports = mongoose.model("usuarios", UsuariosSchema); 
